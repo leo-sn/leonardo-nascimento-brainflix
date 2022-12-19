9ede650b8765ba6a92574eca6839c2213c44c170
Leo Nascimento <95266043+leo-sn@users.noreply.github.com>
2022-12-18 17:55:07 -0800

    upated links to localhost node server

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
   const [apiKey, setApiKey] = useState(null);
   const { videoId } = useParams();
   
-
+  //getting an API key when the app starts
   useEffect(() => {
       axios.get('https://project-2-api.herokuapp.com/register')
       .then((res) => {
@@ -21,27 +21,45 @@ function Home() {
       )
   },[])
 
+  // if apiKey exists, call axios and setVideoSelected
   useEffect(() => {
-    if(apiKey){ axios.get(`https://project-2-api.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8?api_key=${apiKey}`)
+    if(apiKey){ axios.get(`http://localhost:3030/videos/84e96018-4022-434e-80bf-000ce4cd12b8`)
     .then(res => setVideoSelected(res.data))}
   },[apiKey])
+
+  // useEffect(() => {
+  //   if(apiKey){ axios.get(`https://project-2-api.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8?api_key=${apiKey}`)
+  //   .then(res => setVideoSelected(res.data))}
+  // },[apiKey])
 
   useEffect(() => {
     apiKey && getVideosList()
   },[apiKey])
 
   function getVideosList() {
-    axios.get(`https://project-2-api.herokuapp.com/videos?api_key=${apiKey}`)
+    axios.get(`http://localhost:3030/videos`)
     .then((res) => {
       setVideos(res.data)
     })
   }
+  // function getVideosList() {
+  //   axios.get(`https://project-2-api.herokuapp.com/videos?api_key=${apiKey}`)
+  //   .then((res) => {
+  //     setVideos(res.data)
+  //   })
+  // }
 
   useEffect( () => {
-    axios.get(`https://project-2-api.herokuapp.com/videos/${videoId}?api_key=${apiKey}`)
+    axios.get(`http://localhost:3030/videos/${videoId}`)
     .then(res => setVideoSelected(res.data))
     window.scrollTo(0, 0);
-  }, [apiKey, videoId])
+  }, [videoId])
+
+  // useEffect( () => {
+  //   axios.get(`https://project-2-api.herokuapp.com/videos/${videoId}?api_key=${apiKey}`)
+  //   .then(res => setVideoSelected(res.data))
+  //   window.scrollTo(0, 0);
+  // }, [apiKey, videoId])
 
 
   return (
